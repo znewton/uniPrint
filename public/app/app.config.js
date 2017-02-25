aeabd77e1fdc314c5915520d8f2ba6df9acc1d57
Figgynewts <znewton@iastate.edu>
2017-02-25 01:08:01 -0600

    Navbar looks good enough for now

--- public/app/app.config.js
+++ public/app/app.config.js
@@ -1,7 +1,15 @@
-app.config(function($routeProvider) {
+app.config(function($routeProvider, $mdThemingProvider) {
 	$routeProvider
 		.when("/", {
 			template: "<home></home>"
 		})
 		.otherwise({redirectTo:'/404'});
+
+	$mdThemingProvider.theme('default')
+		.primaryPalette('blue-grey')
+		// If you specify less than all of the keys, it will inherit from the
+		// default shades
+		.accentPalette('light-blue');
+
+
 });
