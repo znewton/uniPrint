6ec96390768609c13f7977ba8d4e335977793e87
Phippen <phippen@iastate.edu>
2017-02-25 21:04:17 -0600

    Plz?

---
+++ public/app/components/request-index/request-index.module.js
@@ -0,0 +1,18 @@
+angular.module('requestIndex', ['requestCard']).component('requestIndex',{
+	templateUrl: 'app/components/request-index/request-index.template.html',
+	controller: function RequestIndexController($scope, $timeout) {
+
+		$scope.requests = {};
+		$scope.loading = true;
+
+		$timeout(function() {
+			for(var key in fake_db_print_requests) {
+				if(fake_db_print_requests.hasOwnProperty(key)) {
+					$scope.requests[key] = fake_db_print_requests[key];
+				}
+			}
+			$scope.loading = false;
+			console.log($scope.requests);
+		}, 200);
+	}
+});
